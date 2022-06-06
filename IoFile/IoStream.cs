d491676f579fcbe0754cc248c70ca38572b75377
mohan338 <sahumohan338@gmail.com>
2022-06-06 17:50:32 +0530

    [mohan]add: File Exists or not

---
+++ IoFile/IoStream.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoFile
+{
+    public class IoStream
+    {
+        string Path = @"D:\dotnet\Files\InputOutputStream\IoFile\File.txt";
+        public void FileExists()
+        {
+            if(File.Exists(Path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IoFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using IoFile;
+using System;
+
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome to the Input Output Program");
+        Console.WriteLine("1.Enter 1 File exists or not\n 2.Enter 2 exit");
+        bool check=true;
+        IoStream Files = new IoStream();
+        while(check)
+        {
+            Console.WriteLine("\nEnter Above Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    Files.FileExists();
+                    break;
+                case 2:
+                    check = false;
+                    break;
+                  
+            }
+        }
+    }
+}
